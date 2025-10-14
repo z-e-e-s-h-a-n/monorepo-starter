65676c309efa6cec550ffc6def9ea89fff9d5864
Zeeshan Asghar <mzeeshanff10@gmail.com>
2025-10-14 16:40:15 +0400

    feat(server): add common modules, env validation, winston logger, public module, and global filters/interceptors

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,23 @@
-import { Module } from '@nestjs/common';
- 
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { EnvModule } from "@/modules/env/env.module";
+import { validateEnv } from "@/schemas/env.schema";
+import { PublicModule } from "@/modules/public/public.module";
+import { LoggerModule } from "@/modules/logger/logger.module";
+import { AllExceptionsFilter } from "@/common/filters/exceptions.filter";
+import { ResponseInterceptor } from "@/common/interceptors/response.interceptor";
+
 @Module({
-  imports: [],
-  })
+  imports: [
+    PublicModule,
+    LoggerModule,
+    EnvModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: validateEnv,
+    }),
+  ],
+
+  providers: [AllExceptionsFilter, ResponseInterceptor],
+})
 export class AppModule {}
